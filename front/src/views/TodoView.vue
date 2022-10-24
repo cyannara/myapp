8cfdf760e2a356e1b801357ff2af658ca918d14e
cyannara <cyannara@naver.com>
2022-10-25 06:42:57 +0900

    front

---
+++ front/src/views/TodoView.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>todolist</h1>
+    <table class='table'>
+      <thead>
+        <tr>
+          <th>번호</th>
+          <th>내용</th>
+          <th>완 료 ~~여부</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for='(todo) in todoList' :key='todo.no'>
+          <td>{{todo.no}}</td>
+          <td>{{todo.contents}}</td>
+          <td>{{todo.todoYn}}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  data () {
+    return {
+      todoList: []
+    }
+  },
+  mounted () {
+    this.getList()
+  },
+  methods: {
+    async getList () {
+      const response = await axios.get('/api/todoSelect')
+      this.todoList = response.data
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/main/java/com/yedam/myserver/todo/web/TodoController.java
+++ src/main/java/com/yedam/myserver/todo/web/TodoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yedam.myserver.todo.mapper.TodoMapper;
@@ -13,6 +14,7 @@ import com.yedam.myserver.todo.vo.TodoVO;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping("/api")
 public class TodoController {
 
 	@Autowired TodoMapper mapper;
